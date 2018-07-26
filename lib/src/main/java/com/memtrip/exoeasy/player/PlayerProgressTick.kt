3e579ea0bc1103208ea9fba1cea0aae18019a2b2
Samuel Kirton <samkirton@me.com>
2018-07-26 17:04:12 +0100

    * Added an apache licence header to the top of each class * init README

--- lib/src/main/java/com/memtrip/exoeasy/player/PlayerProgressTick.kt
+++ lib/src/main/java/com/memtrip/exoeasy/player/PlayerProgressTick.kt
@@ -3,6 +3,21 @@ package com.memtrip.exoeasy.player
 import android.os.Handler
 import com.google.android.exoplayer2.ExoPlayer
 
+/**
+ * Copyright 2013-present memtrip LTD.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 internal class PlayerProgressTick(
     private val player: ExoPlayer,
     private val progressTracker: PlayerProgressTracker,
